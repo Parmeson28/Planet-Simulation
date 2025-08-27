d49145e6f5f9b3889f9b00db253a5197d358cdff
Parmeson28 <tiagopassigern2802@gmail.com>
2025-08-26 22:21:10 -0300

    Rendering model (not working yet)

--- core/src/main/java/com/javaProjects/game/Main.java
+++ core/src/main/java/com/javaProjects/game/Main.java
@@ -1,124 +1,71 @@
 package com.javaProjects.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
+import com.badlogic.gdx.graphics.g3d.utils.AnimationController;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter implements InputProcessor{
+public class Main extends ApplicationAdapter{
+
     private PerspectiveCamera camera;
     private ModelBatch modelBatch;
-    private ModelBuilder modelBuilder;
-    private Model box;
+    private Model model;
     private ModelInstance modelInstance;
     private Environment environment;
+    private AnimationController controller;
 
     @Override
     public void create() {
         camera = new PerspectiveCamera(75, 
-                                        Gdx.graphics.getWidth(),
+                                        Gdx.graphics.getWidth(), 
                                         Gdx.graphics.getHeight());
-        camera.position.set(0f, 0f, 3f);
-        camera.lookAt(0f, 0f, 0f);
+        camera.position.set(0f, 100f, 100f);
+        camera.lookAt(0f, 100f, 0f);
+
         camera.near = 0.1f;
         camera.far = 300f;
-
-        modelBatch = new ModelBatch();
-        modelBuilder = new ModelBuilder();
-        box = modelBuilder.createBox(2f, 2f, 2f, 
-                                    new Material(ColorAttribute.createDiffuse(Color.BLUE)),
-                                    VertexAttributes.Usage.Position|VertexAttributes.Usage.Normal);
         
-        modelInstance = new ModelInstance(box, 0f, 0f, 0f);
+        modelBatch = new ModelBatch();
+
+        ObjLoader modelLoader = new ObjLoader();
+        model = modelLoader.loadModel(Gdx.files.getFileHandle("walking_3.obj", Files.FileType.Internal));
+        
+        modelInstance = new ModelInstance(model);
+
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.8f, 0.8f, 0.8f, 1f));
-        
-        Gdx.input.setInputProcessor(this);
+
+        controller = new AnimationController(modelInstance);
+        //controller.setAnimation("Mixamo.com", -1);
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT|GL20.GL_DEPTH_BUFFER_BIT);
-        
+
         camera.update();
+        controller.update(Gdx.graphics.getDeltaTime());
 
         modelBatch.begin(camera);
-        modelBatch.render(modelInstance);
+        modelBatch.render(modelInstance, environment);
         modelBatch.end();
     }
 
-
-    @Override
-    public boolean keyDown(int keycode) {
-        if (keycode == Input.Keys.LEFT)
-            camera.rotateAround(new Vector3(0f, 0f, 0f), new Vector3(0f, 1f, 0f), 1f);
-        if (keycode == Input.Keys.RIGHT)
-            camera.rotateAround(new Vector3(0f, 0f, 0f), new Vector3(0f, 1f, 0f), -1f);
-
-        return true;
-    }
-
-
     @Override
     public void dispose() {
-        box.dispose();
         modelBatch.dispose();
+        model.dispose();
     }
-
-    
-    
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        return false;
-    }
+   
 }
