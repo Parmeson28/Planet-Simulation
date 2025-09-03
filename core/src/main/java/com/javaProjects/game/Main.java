cb98683c5b93de240844a15b0c129ff54de48014
Parmeson28 <tiagopassigern2802@gmail.com>
2025-09-02 22:20:06 -0300

    Rendering 2 planet spheres and 1 star sphere. Commented the code and added FPS counter

--- core/src/main/java/com/javaProjects/game/Main.java
+++ core/src/main/java/com/javaProjects/game/Main.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
@@ -26,6 +28,7 @@ public class Main extends ApplicationAdapter{
     private ModelBatch modelBatch;
     private ModelBuilder modelBuilder;
     private Model model;
+    private ModelInstance star, planet;
     private Array<ModelInstance> instances;
 
     //Environment Variables
@@ -37,55 +40,122 @@ public class Main extends ApplicationAdapter{
     private float cameraVel = 0.05f;
     private Vector3 cameraRotation = new Vector3(0f, 1f, 0f);
 
+    //FPS counter
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private boolean showFPS = true;
+
     @Override
     public void create() {
-        
+
+        /******** === OVERALL VIEW OF THE CODE === ********/
+        /******** ===== (CREATING THE CAMERA) ===== ********/
+
+        //Setting the camera as PerspectiveCamera for a 3d impression
         camera = new PerspectiveCamera(75, 
                                        Gdx.graphics.getWidth(), 
                                        Gdx.graphics.getHeight());
 
-        camera.position.set(0f, 0f, 0f);
+        //Setting the camera position and where it looks at
+        camera.position.set(0f, 10f, 0f);
         camera.lookAt(0f, 0f, 0f);
+        //Setting how far and how near we need 
+        //to be to stop rendering the models
         camera.near = 0.1f;
         camera.far = 300f;
 
+        /******** ===== (CREATING THE CAMERA) ===== ********/
+        /******** === OVERALL VIEW OF THE CODE === ********/
 
-        instances = new Array<>();
-        modelBatch = new ModelBatch();
 
-        //Building the models
+
+        /******** ============ OVERALL VIEW OF THE CODE ============ ********/
+        /******** ===== (BUILDING THE PLANET AND STAR MODELS) ===== ********/
+
+        //The modelBuilder is used to build the sphere model and passes the node id 
+        //which later will be used to identify the type of body it is
+
         modelBuilder = new ModelBuilder();
+        instances = new Array<>();
 
         modelBuilder.begin();
-
         modelBuilder.node().id = "sun";
         modelBuilder.part("sphere", GL20.GL_TRIANGLES, Usage.Position|Usage.Normal, 
                           new Material(ColorAttribute.createDiffuse(Color.YELLOW))).sphere(1f, 1f, 1f, 40, 40);
-        
         model = modelBuilder.end();
 
-        ModelInstance suInstance = new ModelInstance(model);
+        star = new ModelInstance(model);
+        instances.add(star);
 
-        instances.add(suInstance);
+        modelBuilder.begin();
+        modelBuilder.node().id = "planet";
+        modelBuilder.part("sphere", GL20.GL_TRIANGLES, Usage.Position|Usage.Normal, 
+                          new Material(ColorAttribute.createDiffuse(Color.GREEN))).sphere(1f, 1f, 1f, 40, 40);
+        model = modelBuilder.end();
+
+        planet = new ModelInstance(model);
+        planet.transform.setToTranslation(5f, 0f, 5f);
+        instances.add(planet);
+
+        modelBuilder.begin();
+        modelBuilder.node().id = "planet";
+        modelBuilder.part("sphere", GL20.GL_TRIANGLES, Usage.Position|Usage.Normal, 
+                          new Material(ColorAttribute.createDiffuse(Color.BLUE))).sphere(1f, 1f, 1f, 40, 40);
+        model = modelBuilder.end();
+        planet = new ModelInstance(model);
+        planet.transform.setToTranslation(5f, 0f, 2f);
+        instances.add(planet);
+
+        System.out.println(instances.size);
+        
+        /******** ===== (BUILDING THE PLANET AND STAR MODELS) ===== ********/
+        /******** ============ OVERALL VIEW OF THE CODE ============ ********/
+
+
+        /******** ========== OVERALL VIEW OF THE CODE ========== ********/
+        /******** ===== (SETTING ENVIRONMENT AND LIGHTING) ===== ********/
 
         //Setting the directional light
         directionalLight = new DirectionalLight();
-        directionalLight.set(1f, 1f, 1f, 0f, 5f, 0f);
+        directionalLight.set(1f, 1f, 1f, 0f, -2f, 0f);
 
-        //Setting environment
+        //Setting environment and passing the 
+        //directional light created before
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
         environment.add(directionalLight);
+
+        //FPS counter
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        
+        /******** ===== (SETTING ENVIRONMENT AND LIGHTING) ===== ********/
+        /******** ========== OVERALL VIEW OF THE CODE ========== ********/
+
+        modelBatch = new ModelBatch();
     }
 
 
     //MAIN LOOP FOR THE APPLICATION TO RUN
     @Override
     public void render() {
+
         Gdx.gl.glClearColor(0, 0, 0, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT|GL20.GL_DEPTH_BUFFER_BIT);
 
+        //Drawing FPS
+        batch.begin();
+        font.draw(batch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 10, 20);
+        batch.end();
+
         camera.update();
+
+        //Searching for planet models in the instances
+        for(ModelInstance obj : instances){
+            if(obj.getNode("planet") != null)
+                obj.transform.translate(0f, 0.01f, 0f);
+        }
+        
 
         modelBatch.begin(camera);
         modelBatch.render(instances, environment);
@@ -122,6 +192,10 @@ public class Main extends ApplicationAdapter{
     
     @Override
     public void dispose() {
+        
+        font.dispose();
+        batch.dispose();
+
         modelBatch.dispose();
         model.dispose();
     }
